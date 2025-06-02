841a9bcb37b00e0f960d7cdd5a9862efd7f4bbba
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-06-02 16:52:51 -0400

    Layout updates

--- client/howler_client/client.py
+++ client/howler_client/client.py
@@ -1,5 +1,5 @@
-from howler_client.connection import Connection
 from howler_client.common.utils import walk_api_path
+from howler_client.connection import Connection
 from howler_client.module.bundle import Bundle
 from howler_client.module.help import Help
 from howler_client.module.hit import Hit
@@ -8,6 +8,8 @@ from howler_client.module.user import User
 
 
 class Client(object):
+    "Main howler client object, wrapping API calls"
+
     def __init__(self, connection):
         self._connection: Connection = connection
 
@@ -20,6 +22,4 @@ class Client(object):
         paths = []
         walk_api_path(self, [""], paths)
 
-        self.__doc__ = "Client provides the following methods:\n\n" + "\n".join(
-            ["\n".join(p + [""]) for p in paths]
-        )
+        self.__doc__ = "Client provides the following methods:\n\n" + "\n".join(["\n".join(p + [""]) for p in paths])
