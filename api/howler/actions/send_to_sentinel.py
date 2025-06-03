4f4b58b5de42ac2acea20af99ed4303cc08bea45
Emil Norman <emil.norman@truesec.se>
2025-06-03 15:42:54 -0400

    Fix PR comments

--- api/howler/actions/send_to_sentinel.py
+++ api/howler/actions/send_to_sentinel.py
@@ -18,89 +18,80 @@ def execute(query: str, **kwargs):
     report = []
     ds = datastore()
 
-    hit: Hit = ds.hit.search(query, as_obj=True)["items"]
-    if not hit:
+    hits: list[Hit] = ds.hit.search(query, as_obj=True)["items"]
+    if not hits:
         report.append(
             {
                 "query": query,
                 "outcome": "error",
-                "title": "Invalid Hit",
-                "message": f"Hit with ID {query} does not exist.",
-            }
-        )
-        return report
-    if len(hit) > 1:
-        report.append(
-            {
-                "query": query,
-                "outcome": "error",
-                "title": "Multiple Hits Found",
-                "message": f"Multiple hits found for query {query}. Please refine your query.",
-            }
-        )
-        return report
-    hit = hit[0]
-
-    # Get bearer token
-    try:
-        credentials = json.loads(os.environ['HOWLER_SENTINEL_INGEST_CREDENTIALS'])
-    except (KeyError, json.JSONDecodeError):
-        report.append(
-            {
-                "query": query,
-                "outcome": "error",
-                "title": "Invalid Credentials",
-                "message": "Environment variable HOWLER_GRAPH_ALERT_CREDENTIALS is invalid or not set.",
+                "title": "No hits returned by query",
+                "message": f"No hits returned by '{query}'",
             }
         )
         return report
 
-    token_request_url = f"https://login.microsoftonline.com/{hit.azure.tenant_id}/oauth2/v2.0/token"
-    data = {
-        'grant_type': 'client_credentials',
-        'client_id': credentials['client_id'],
-        'client_secret': credentials['client_secret'],
-        'scope': 'https://monitor.azure.com/.default'
-    }
-    response = requests.post(token_request_url, data=data)
-    if response.status_code != 200:
-        report.append(
-            {
-                "query": query,
-                "outcome": "error",
-                "title": "Authentication failed",
-                "message": f"Authentication to Microsoft Graph API failed with status code {response.status_code}.",
-            }
-        )
-        return report
-    token = response.json()["access_token"]
+    for hit in hits:
+        # Get bearer token
+        # A token broker should be used to avoid hitting API limits.
+        try:
+            credentials = json.loads(os.environ['HOWLER_SENTINEL_INGEST_CREDENTIALS'])
+        except (KeyError, json.JSONDecodeError):
+            report.append(
+                {
+                    "query": query,
+                    "outcome": "error",
+                    "title": "Invalid Credentials",
+                    "message": "Environment variable HOWLER_SENTINEL_INGEST_CREDENTIALS is invalid or not set.",
+                }
+            )
+            continue
 
-    uri = f"https://{credentials['dce']}.ingest.monitor.azure.com/dataCollectionRules/{credentials['dcr']}/" + \
-          f"streams/{credentials['table']}?api-version=2021-11-01-preview"
-
-    payload = [
-        {
-            "TimeGenerated": hit.event.ingested.isoformat(),
-            "Title": hit.howler.analytic,
-            "RawData": {"Hit": hit.as_primitives(), "From": "Howler"}
+        token_request_url = f"https://login.microsoftonline.com/{hit.azure.tenant_id}/oauth2/v2.0/token"
+        data = {
+            'grant_type': 'client_credentials',
+            'client_id': credentials['client_id'],
+            'client_secret': credentials['client_secret'],
+            'scope': 'https://monitor.azure.com/.default'
         }
-    ]
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json'
-    }
+        response = requests.post(token_request_url, data=data)
+        if not response.ok:
+            report.append(
+                {
+                    "query": query,
+                    "outcome": "error",
+                    "title": "Authentication failed",
+                    "message": f"Authentication to Azure Monitor API failed with status code {response.status_code}.",
+                }
+            )
+            continue
+        token = response.json()["access_token"]
 
-    response = requests.post(uri, headers=headers, json=payload)
-    if response.status_code != 204:
-        report.append(
+        uri = f"https://{credentials['dce']}.ingest.monitor.azure.com/dataCollectionRules/{credentials['dcr']}/" + \
+            f"streams/{credentials['table']}?api-version=2021-11-01-preview"
+
+        payload = [
             {
-                "query": query,
-                "outcome": "error",
-                "title": "Azure RM API request failed",
-                "message": f"POST request to Azure RM failed with status code {response.status_code}.",
+                "TimeGenerated": hit.event.ingested.isoformat(),
+                "Title": hit.howler.analytic,
+                "RawData": {"Hit": hit.as_primitives(), "From": "Howler"}
             }
-        )
-        return report
+        ]
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.post(uri, headers=headers, json=payload)
+        if not response.ok:
+            report.append(
+                {
+                    "query": query,
+                    "outcome": "error",
+                    "title": "Azure Monitor API request failed",
+                    "message": f"POST request to Azure Monitor failed with status code {response.status_code}.",
+                }
+            )
+            continue
 
     return report
 
