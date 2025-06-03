5769aa3377184f015c542fabcc9489000bc03143
Emil Norman <emil.norman@truesec.se>
2025-06-03 11:05:03 -0400

    Add an action to sync status and assessment from Howler to Defender XDR

---
+++ api/howler/actions/update_defender_xdr_alert.py
@@ -0,0 +1,176 @@
+import json
+import os
+import requests
+
+from howler.common.loader import datastore
+from howler.odm.models.action import VALID_TRIGGERS
+from howler.odm.models.hit import Hit
+from howler.odm.models.howler_data import Assessment, HitStatus
+
+OPERATION_ID = "update_defender_xdr_alert"
+
+map = {
+    "graph": {
+        "status": {
+            HitStatus.OPEN: "new",
+            HitStatus.IN_PROGRESS: "inProgress",
+            HitStatus.ON_HOLD: "inProgress",
+            HitStatus.RESOLVED: "resolved",
+        },
+        "classification": {
+            Assessment.AMBIGUOUS: "unknown",
+            Assessment.SECURITY: "informationalExpectedActivity",
+            Assessment.DEVELOPMENT: "informationalExpectedActivity",
+            Assessment.FALSE_POSITIVE: "falsePositive",
+            Assessment.LEGITIMATE: "informationalExpectedActivity",
+            Assessment.TRIVIAL: "falsePositive",
+            Assessment.RECON: "truePositive",
+            Assessment.ATTEMPT: "truePositive",
+            Assessment.COMPROMISE: "truePositive",
+            Assessment.MITIGATED: "truePositive",
+            None: "unknown",
+        },
+        "determination": {
+            Assessment.AMBIGUOUS: "insufficientData",
+            Assessment.SECURITY: "securityTesting",
+            Assessment.DEVELOPMENT: "confirmedUserActivity",
+            Assessment.FALSE_POSITIVE: "other",
+            Assessment.LEGITIMATE: "lineOfBusinessApplication",
+            Assessment.TRIVIAL: "other",
+            Assessment.RECON: "multiStagedAttack",
+            Assessment.ATTEMPT: "other",
+            Assessment.COMPROMISE: "maliciousUserActivity",
+            Assessment.MITIGATED: "other",
+            None: "unknown",
+        },
+    },
+}
+
+def execute(query: str, **kwargs):
+    """Update Microsoft Defender XDR alert.
+
+    Args:
+        query (str): The query on which to apply this automation.
+    """
+
+    report = []
+    ds = datastore()
+
+    hit: Hit = ds.hit.search(query, as_obj=True)["items"][0]
+
+    if not hit:
+        report.append(
+            {
+                "query": query,
+                "outcome": "error",
+                "title": "Invalid Hit",
+                "message": f"Hit with ID {query} does not exist.",
+            }
+        )
+        return report
+
+    # Get bearer token
+    try:
+        credentials = json.loads(os.environ['HOWLER_GRAPH_ALERT_CREDENTIALS'])
+    except KeyError:
+        report.append(
+            {
+                "query": query,
+                "outcome": "error",
+                "title": "Invalid Credentials",
+                "message": "Environment variable HOWLER_GRAPH_ALERT_CREDENTIALS is invalid or not set.",
+            }
+        )
+        return report
+
+    token_request_url = f"https://login.microsoftonline.com/{hit.azure.tenant_id}/oauth2/v2.0/token"
+    data = f"grant_type=client_credentials&client_id={credentials['client_id']}" + \
+           f"&client_secret={credentials['client_secret']}&scope=https%3A%2F%2Fgraph.microsoft.com%2F.default"
+    response = requests.post(token_request_url, data=data)
+
+    if response.status_code != 200:
+        report.append(
+            {
+                "query": query,
+                "outcome": "error",
+                "title": "Authentication failed",
+                "message": f"Authentication to Microsoft Graph API failed with status code {response.status_code}.",
+            }
+        )
+        return report
+
+    token = response.json()["access_token"]
+
+    # Fetch alert details
+    alert_url = f"https://graph.microsoft.com/v1.0/security/alerts_v2/{hit.rule.id}"
+    response = requests.get(alert_url, headers={"Authorization": f"Bearer {token}"})
+    if response.status_code != 200:
+        report.append(
+            {
+                "query": query,
+                "outcome": "error",
+                "title": "Microsoft Graph API request failed",
+                "message": f"GET request to Microsoft Graph failed with status code {response.status_code}.",
+            }
+        )
+        return report
+    alert_data = response.json()
+
+    # Update alert
+    if "assessment" in hit.howler and hit.howler.assessment in map["graph"]["classification"] and \
+       hit.howler.assessment in map["graph"]["determination"]:
+        if hit.howler.assessment is None:
+            classification = map["graph"]["classification"][hit.howler.assessment]
+            determination = map["graph"]["determination"][hit.howler.assessment]
+        else:
+            classification = map["graph"]["classification"][hit.howler.assessment]
+            determination = map["graph"]["determination"][hit.howler.assessment]
+    else:
+        classification = alert_data["classification"]
+        determination = alert_data["determination"]
+
+    status = map["graph"]["status"][hit.howler.status]
+    assigned_to = alert_data["assignedTo"]
+
+    data = f"""{{
+        "assignedTo": "{assigned_to}",
+        "classification": "{classification}",
+        "determination": "{determination}",
+        "status": "{status}"
+    }}"""
+
+    response = requests.patch(alert_url, data = data,
+                              headers={"Authorization": f"Bearer {token}", "Content-Type": "application/json"})
+    if response.status_code != 200:
+        report.append(
+            {
+                "query": query,
+                "outcome": "error",
+                "title": "Microsoft Graph API request failed",
+                "message": f"PATCH request to Microsoft Graph failed with status code {response.status_code}.",
+            }
+        )
+        return report
+
+    return report
+
+def specification():
+    return {
+        "id": OPERATION_ID,
+        "title": "Update Microsoft Defender XDR alert",
+        "priority": 8,
+        "i18nKey": "Update Microsoft Defender XDR alert",
+        "description": {
+            "short": "Update Microsoft Defender XDR alert",
+            "long": execute.__doc__,
+        },
+        "roles": ["automation_basic"],
+        "steps": [
+            {
+                "args": {},
+                "options": {},
+                "validation": {}
+            }
+        ],
+        "triggers": VALID_TRIGGERS,
+    }
