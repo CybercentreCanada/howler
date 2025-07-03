28574ecb8475b6d9c83bc498371e9391a474ae4b
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-07-03 14:32:38 -0400

    Fixing package build

--- ui/build_scripts/prep_for_publish.py
+++ ui/build_scripts/prep_for_publish.py
@@ -1,37 +1,91 @@
 import json
+import shutil
 from pathlib import Path
 
-src_path = Path(__file__).parent.parent / "src"
+print("Step 1: Copy Files")
 
-package_json = json.loads((src_path.parent / "dist" / "package.json").read_text())
+ui_path = Path(__file__).parent.parent
+
+if not (ui_path / "dist" / ".npmrc").exists():
+    print("\tCopying .npmrc")
+    shutil.copy(ui_path / ".npmrc", ui_path / "dist" / ".npmrc")
+
+if not (ui_path / "dist" / "package.json").exists():
+    print("\tCopying package.json")
+    shutil.copy(ui_path / "package.json", ui_path / "dist" / "package.json")
+
+if not (ui_path / "dist" / "public").exists():
+    print("\tRecursively copying public path")
+    shutil.copytree(ui_path / "public", ui_path / "dist" / "public")
+
+print("Step 2: Prepare package.json")
+
+package_json = json.loads((ui_path / "dist" / "package.json").read_text())
 
 if "devDependencies" in package_json:
-    print("Removing devDependencies key")
+    print("\tRemoving devDependencies key")
     del package_json["devDependencies"]
 
 if "lint" in package_json:
-    print("Removing lint-staged key")
+    print("\tRemoving lint-staged key")
     del package_json["lint-staged"]
 
 if "scripts" in package_json:
-    print("Removing scripts key")
+    print("\tRemoving scripts key")
     del package_json["scripts"]
 
 if "pnpm" in package_json:
-    print("Removing pnpm key")
+    print("\tRemoving pnpm key")
     del package_json["pnpm"]
 
 exports: list[Path] = []
-for path in src_path.rglob("**"):
-    if path == src_path:
+for path in (ui_path / "src").rglob("**"):
+    if path == (ui_path / "src"):
         continue
 
-    exports.append(path.relative_to(src_path))
+    exports.append(path.relative_to(ui_path / "src"))
 
-print(f"Writing {len(exports)} entries to exports")
+print(f"\t Writing {len(exports)} entries to exports")
 
 package_json["exports"] = {f"./{path}/*": f"./{path}/*.js" for path in exports}
 
-(src_path.parent / "dist" / "package.json").write_text(
-    json.dumps(package_json, indent=2)
-)
+(ui_path / "dist" / "package.json").write_text(json.dumps(package_json, indent=2))
+
+print("Step 3: Rewiring imports")
+for js_file in (ui_path / "dist").rglob("**/*.js"):
+    current_content = js_file.read_text()
+
+    print("-" * 80)
+    print("Transforming", js_file)
+
+    for path in exports:
+        if f"'{path}" not in current_content:
+            continue
+
+        print("\tFixing import", path)
+
+        current_content = current_content.replace(
+            f"'{path}", f"'@cccsaurora/howler-ui/{path}"
+        )
+
+    js_file.write_text(current_content)
+
+for js_file in (ui_path / "dist").rglob("**/*.ts"):
+    current_content = js_file.read_text()
+
+    print("-" * 80)
+    print("Transforming", js_file)
+
+    for path in exports:
+        if f"'{path}" not in current_content:
+            continue
+
+        print("\tFixing import", path)
+
+        current_content = current_content.replace(
+            f"'{path}", f"'@cccsaurora/howler-ui/{path}"
+        )
+
+    js_file.write_text(current_content)
+
+print("-" * 80)
