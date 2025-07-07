87563ee072b596408f0a47721df29fdb8a869ca2
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-07-07 13:52:02 -0400

    Add borealis plugin

---
+++ ui/src/plugins/borealis/components/BorealisTypography.tsx
@@ -0,0 +1,58 @@
+import { Typography } from '@mui/material';
+import { EnrichedTypography, useBorealisEnrichSelector, type EnrichedTypographyProps } from 'borealis-ui';
+import type { PluginTypographyProps } from 'components/elements/PluginTypography';
+import { memo, type FC } from 'react';
+
+const BorealisTypography: FC<PluginTypographyProps> = ({ children, value, context, ...props }) => {
+  const guessType = useBorealisEnrichSelector(ctx => ctx.guessType);
+
+  const type = guessType(value);
+
+  if (!type) {
+    return <Typography {...props}>{children}</Typography>;
+  }
+
+  let enrichedProps: EnrichedTypographyProps = {
+    ...props,
+    value
+  };
+
+  if (context === 'banner') {
+    enrichedProps = {
+      ...enrichedProps,
+      slotProps: { stack: { component: 'span' } }
+    };
+  } else if (context === 'outline') {
+    enrichedProps = {
+      ...enrichedProps,
+      hideLoading: true,
+      slotProps: {
+        stack: {
+          sx: { mr: 'auto' },
+          onClick: e => {
+            e.preventDefault();
+            e.stopPropagation();
+          }
+        },
+        popover: {
+          onClick: e => {
+            e.preventDefault();
+            e.stopPropagation();
+          }
+        } as any
+      }
+    };
+  } else if (context === 'table') {
+    enrichedProps = {
+      ...enrichedProps,
+      hideLoading: true,
+      slotProps: {
+        stack: { sx: { width: '100%', '& > p': { textOverflow: 'ellipsis', overflow: 'hidden' } } }
+      }
+    };
+  }
+
+  return <EnrichedTypography {...enrichedProps} type={type} />;
+};
+
+export default memo(BorealisTypography);
