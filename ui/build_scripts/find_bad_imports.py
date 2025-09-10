6da48f5e4bac05b7a76ee2fd3f646f8d0954804d
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-09-10 09:28:53 -0400

    Finaly tweaks

--- ui/build_scripts/find_bad_imports.py
+++ ui/build_scripts/find_bad_imports.py
@@ -8,8 +8,11 @@ from pathlib import Path
 BANNED_STRINGS = [
     (
         "@mui/icons-material/",
-        "Using @mui/icons-material in exported components leads to issues when extending howler's functionality: "
-        "https://stackoverflow.com/questions/78815858/mui-icons-material-vitest-es-module-issue",
+        [
+            "Using @mui/icons-material/SomeIcon in exported components leads to issues when extending howler's "
+            "functionality: https://stackoverflow.com/questions/78815858/mui-icons-material-vitest-es-module-issue ",
+            "Instead, use import { SomeIcon } from '@mui/icons-material'",
+        ],
     )
 ]
 
@@ -23,11 +26,13 @@ for filename in glob.glob(str(lib_dir / "**/*.ts*"), recursive=True):
     _file = Path(filename)
     data = _file.read_text()
 
-    for banned_string, explanation in BANNED_STRINGS:
+    for banned_string, explanations in BANNED_STRINGS:
         if banned_string in data:
             print("failed")
 
-            wrapped_explanation = textwrap.wrap(explanation, width=120)
+            wrapped_explanation = []
+            for explanation in explanations:
+                wrapped_explanation += textwrap.wrap(explanation, width=120)
 
             print(f"ERROR: {_file.relative_to(root)} contains a banned string:")
             print(
