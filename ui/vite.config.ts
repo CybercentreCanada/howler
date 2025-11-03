c567d5c53d700f5cc032dbef9888409853198ae4
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2025-11-03 11:29:03 -0500

    externalize mui and emotion dependencies

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -55,6 +55,7 @@ export default defineConfig(({ mode }) => {
       sourcemap: false,
       outDir: 'build',
       rollupOptions: {
+        external: [/@mui.*/, /@emotion.*/],
         plugins: [],
         output: {
           manualChunks(id) {
