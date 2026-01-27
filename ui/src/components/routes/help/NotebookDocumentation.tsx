fc38d96883a2ee2c967a03a1e78cf4cfb77f5caa
cccs-tsha <taha.shahid@cse-cst.gc.ca>
2026-01-27 14:04:23 -0500

    feat(ui): added documentation editing plugin to return hello world (#134)
    * feat(ui): Adding documentation editor plugin
    
    * Made plugins for documentations
    
    * Fixing linter
    
    * Added specific route checking within plugin
    
    * Removed template plugin

--- ui/src/components/routes/help/NotebookDocumentation.tsx
+++ ui/src/components/routes/help/NotebookDocumentation.tsx
@@ -1,27 +1,35 @@
 import PageCenter from 'commons/components/pages/PageCenter';
 import Markdown from 'components/elements/display/Markdown';
 import { useScrollRestoration } from 'components/hooks/useScrollRestoration';
-import type { FC } from 'react';
+import howlerPluginStore from 'plugins/store';
+import { useMemo, type FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { usePluginStore } from 'react-pluggable';
+import { modifyDocumentation } from 'utils/utils';
 import NOTEBOOK_EN from './markdown/en/notebook.md';
 import NOTEBOOK_FR from './markdown/fr/notebook.md';
 
 const NotebookDocumentation: FC = () => {
   const { i18n } = useTranslation();
+  const pluginStore = usePluginStore();
   useScrollRestoration();
 
-  const markdown = (i18n.language === 'en' ? NOTEBOOK_EN : NOTEBOOK_FR)
-    .replaceAll(
-      '$NBGALLERY_URL',
-      window.location.host.startsWith('localhost')
-        ? 'https://nbgallery.dev.analysis.cyber.gc.ca'
-        : window.location.origin.replace(/howler(-stg)/, 'nbgallery')
-    )
-    .replaceAll('$CURRENT_URL', window.location.origin);
+  const md = useMemo(() => {
+    let markdown = (i18n.language === 'en' ? NOTEBOOK_EN : NOTEBOOK_FR)
+      .replaceAll(
+        '$NBGALLERY_URL',
+        window.location.host.startsWith('localhost')
+          ? 'https://nbgallery.dev.analysis.cyber.gc.ca'
+          : window.location.origin.replace(/howler(-stg)/, 'nbgallery')
+      )
+      .replaceAll('$CURRENT_URL', window.location.origin);
+
+    return modifyDocumentation(markdown, howlerPluginStore, pluginStore);
+  }, [i18n.language, pluginStore]);
 
   return (
     <PageCenter margin={4} width="100%" textAlign="left">
-      <Markdown md={markdown} />
+      <Markdown md={md} />
     </PageCenter>
   );
 };
