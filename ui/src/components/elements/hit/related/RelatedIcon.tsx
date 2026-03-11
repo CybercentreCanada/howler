462b6dc29488fa1268ff01bb7b9ddd43bbde9c02
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2026-03-11 12:45:26 -0400

    fix(ui): more work on the relatedlink component

--- ui/src/components/elements/hit/related/RelatedIcon.tsx
+++ ui/src/components/elements/hit/related/RelatedIcon.tsx
@@ -1,4 +1,4 @@
-import { getIcon, Icon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import Avatar from '@mui/material/Avatar';
 import { useAppTheme } from 'commons/components/app/hooks';
 import { ApiConfigContext } from 'components/app/providers/ApiConfigProvider';
@@ -17,35 +17,38 @@ const RelatedIcon: FC<{ icon?: string; title?: string; href?: string; compact?: 
     return null;
   }
 
-  if (getIcon(icon)) {
-    return <Icon fontSize="1.5rem" icon={icon} />;
-  }
-
+  let avatarImage: string = null;
   const app = config.configuration.ui.apps.find(a => a.name.toLowerCase() === icon?.toLowerCase());
   if (app) {
     // use the image link for the configured related application instead
-    icon = app[`img_${isDark ? 'd' : 'l'}`];
+    avatarImage = app[`img_${isDark ? 'd' : 'l'}`];
+  } else if (icon.startsWith('http')) {
+    avatarImage = icon;
   }
 
-  return (
-    <Avatar
-      variant="rounded"
-      alt={title ?? href}
-      src={icon}
-      sx={[
-        theme => ({
-          width: theme.spacing(compact ? 4 : 6),
-          height: theme.spacing(compact ? 4 : 6),
-          '& img': {
-            objectFit: 'contain'
-          }
-        }),
-        !icon && { backgroundColor: 'transparent' }
-      ]}
-    >
-      {icon}
-    </Avatar>
-  );
+  if (avatarImage) {
+    return (
+      <Avatar
+        variant="rounded"
+        alt={title ?? href}
+        src={avatarImage}
+        sx={[
+          theme => ({
+            width: theme.spacing(compact ? 4 : 6),
+            height: theme.spacing(compact ? 4 : 6),
+            '& img': {
+              objectFit: 'contain'
+            }
+          }),
+          !avatarImage && { backgroundColor: 'transparent' }
+        ]}
+      >
+        {avatarImage}
+      </Avatar>
+    );
+  }
+
+  return <Icon fontSize="1.5rem" icon={icon} />;
 };
 
 export default RelatedIcon;
