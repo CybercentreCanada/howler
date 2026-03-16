11815c5120b29d506d180f802c9597f4188192bc
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2026-03-16 08:19:48 -0400

    another typed_dict thing

--- api/howler/datastore/types.py
+++ api/howler/datastore/types.py
@@ -1,14 +1,17 @@
+import sys
 from typing import Generic, TypedDict, TypeVar
+
+if sys.version_info >= (3, 11):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
 
 SearchResultType = TypeVar("SearchResultType")
 
 
-class _SearchResult(TypedDict, Generic[SearchResultType]):
+class SearchResult(TypedDict, Generic[SearchResultType]):
     offset: int
     rows: int
     total: int
     items: list[SearchResultType]
-
-
-class SearchResult(_SearchResult[SearchResultType], total=False):
-    next_deep_paging_id: str | None
+    next_deep_paging_id: NotRequired[str | None]
