11811bd1235671b8e6da708e2871ea57651136bd
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2026-03-17 11:30:07 -0400

    more 3.10 shenanigans
    Related Issue: SPBK-3943

--- api/howler/odm/models/case.py
+++ api/howler/odm/models/case.py
@@ -1,4 +1,14 @@
-from enum import StrEnum
+import sys
+
+if sys.version_info >= (3, 11):
+    from enum import StrEnum
+else:
+    from enum import Enum
+
+    class StrEnum(str, Enum):  # type: ignore[no-redef]
+        pass
+
+
 from typing import Optional
 
 from howler import odm
