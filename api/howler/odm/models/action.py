b69897a44aae515d270b92938a190f25f00123e5
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-04-28 09:41:06 -0400

    made the owner, admin and member a list in the odm class of action
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions

--- api/howler/odm/models/action.py
+++ api/howler/odm/models/action.py
@@ -18,19 +18,22 @@ class Operation(odm.Model):
 @odm.model(index=True, store=True, description="Model of actions")
 class Action(odm.Model):
     action_id: str = odm.UUID(description="A UUID for this action")
-    owner_id: str = odm.Keyword(description="The id of the user that created this action")
     # TODO: AG : here as well same as the other ones. This is a highlight. Will try and ask more question on monday.
-    member_id: str = (
-        odm.Keyword(
-            description="group of person to whom can modify this action.",
-            optional=True,
-        ),
+    owner_id: str = odm.List(
+        odm.Keyword(), description="The id of the user that created this action", default=[], optional=True
     )
-    admin_id: str = (
-        odm.Keyword(
-            description="group of person to whom can administer this action.",
-            optional=True,
-        ),
+
+    member_id: list[str] = odm.List(
+        odm.Keyword(),
+        description="group of person to whom can modify this action.",
+        default=[],
+        optional=True,
+    )
+    admin_id: str = odm.List(
+        odm.Keyword(),
+        description="group of person to whom can administer this action.",
+        default=[],
+        optional=True,
     )
     name: str = odm.Keyword(description="The name of the action.")
     query: str = odm.Keyword(description="The query this action is run against.")
