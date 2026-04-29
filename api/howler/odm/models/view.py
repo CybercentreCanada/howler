38ec030744135f4314552be1b8add8d8515d7936
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-04-29 13:58:35 -0400

    correcting view function for the mapping
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions
    
    (cherry picked from commit 759efa45e76c5351f496d0cca8291f84a72199f5)

--- api/howler/odm/models/view.py
+++ api/howler/odm/models/view.py
@@ -71,3 +71,10 @@ class View(odm.Model):
     settings: Settings = odm.Compound(
         Settings, description="Additional View Settings", default={"advance_on_triage": False}
     )
+
+    def get_priv_map(self) -> dict:
+        return {
+            "administrator": self.admin,
+            "member": self.member,
+            "owner": self.owner,
+        }
