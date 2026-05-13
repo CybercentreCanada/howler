1ac649496333cbf8856c33feac4c907f95882306
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-05-13 13:37:10 -0400

    is that the double key?
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions

--- api/howler/odm/random_data.py
+++ api/howler/odm/random_data.py
@@ -176,11 +176,6 @@ def create_users(ds):
                     "password": user_hash,
                 },
             },
-            "type": [
-                "user",
-                "automation_basic",
-                "actionrunner_basic",
-            ],
             "password": user_hash,
             "uname": "user",
             "favourite_views": [user_view.view_id],
