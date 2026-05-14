ba72b937016ff4ff7f8b9e2d0d7ba335db5aab26
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-05-14 08:25:39 -0400

    type ignore on lazy import to shut down the type checking
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions

--- api/howler/api/v1/auth.py
+++ api/howler/api/v1/auth.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 
 from authlib.integrations.base_client import OAuthError
 from flask import current_app, request
-from passlib.hash import bcrypt
+from passlib.hash import bcrypt  # type: ignore
 
 import howler.services.auth_service as auth_service
 import howler.services.user_service as user_service
