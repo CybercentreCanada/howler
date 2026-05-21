700a2032856b8bf6d3d5c95c9fc67715453173eb
Xuan Pham <xuan.pham@cyber.gc.ca>
2026-05-21 15:45:27 -0400

    fix left nav spacing and don't skip favourite views refresh

--- ui/src/commons/components/app/AppDefaults.ts
+++ ui/src/commons/components/app/AppDefaults.ts
@@ -30,7 +30,7 @@ export const AppDefaultsPreferencesConfigs: AppPreferenceConfigs = {
 // AppLeftNavConfigs defaults.
 export const AppDefaultsLeftNavConfigs: AppLeftNavConfigs = {
   elements: [],
-  width: 240,
+  width: 280,
   hideNestedIcons: false
 };
 
