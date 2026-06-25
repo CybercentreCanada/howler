07fccc432eb4793ea05c83e2b4fff3093b8ef537
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-06-25 13:41:28 -0400

    hopefully compatible again
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions
    
    (cherry picked from commit cfd034c184c2b874856a06774795b38826e8d6e0)

--- api/howler/security/utils.py
+++ api/howler/security/utils.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 from urllib.parse import urlparse
 
 from opentelemetry import trace
-from passlib.handlers.bcrypt import bcrypt
+from passlib.hash import bcrypt
 
 from howler.common.exceptions import HowlerValueError
 from howler.config import config
