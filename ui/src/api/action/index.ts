8bc4702db95d24b3421c73edf5682b613b633162
agauvin <alexandre.gauvin@cse-cst.gc.ca>
2026-06-26 13:07:33 -0400

    alligned back action index with develop. moved something by mistake
    Signed-off-by: agauvin <alexandre.gauvin@cse-cst.gc.ca>
    
    Related Issue: multi_owner-view-dossier-actions

--- ui/src/api/action/index.ts
+++ ui/src/api/action/index.ts
@@ -1,4 +1,14 @@
-import { hdelete, hget, hpatch, hpost, hput, joinAllUri, joinUri, uri as parentUri } from 'api';
+import {
+  hdelete,
+  hget,
+  hpatch,
+  hpost,
+  hput,
+  joinAllUri,
+  joinUri,
+  uri as parentUri,
+  type HowlerRefreshParam
+} from 'api';
 import * as execute from 'api/action/execute';
 import * as operations from 'api/action/operations';
 import { action } from 'api/search';
@@ -17,20 +27,20 @@ export const get = (id: string): Promise<Action> => {
     .then(res => res.items[0]);
 };
 
-export const post = (data: Action): Promise<Action> => {
-  return hpost(uri(), data);
+export const post = (data: Action, refresh?: HowlerRefreshParam): Promise<Action> => {
+  return hpost(uri(), data, {}, refresh ? new URLSearchParams({ refresh }) : undefined);
 };
 
-export const put = (id: string, data: Action): Promise<Action> => {
-  return hput(uri(id), data);
+export const put = (id: string, data: Action, refresh?: HowlerRefreshParam): Promise<Action> => {
+  return hput(uri(id), data, {}, refresh ? new URLSearchParams({ refresh }) : undefined);
 };
 
-export const patch = (id: string, data: Action): Promise<Action> => {
-  return hpatch(uri(id), data);
+export const patch = (id: string, data: Action, refresh?: HowlerRefreshParam): Promise<Action> => {
+  return hpatch(uri(id), data, {}, refresh ? new URLSearchParams({ refresh }) : undefined);
 };
 
-export const del = (id: string): Promise<void> => {
-  return hdelete(uri(id));
+export const del = (id: string, refresh?: HowlerRefreshParam): Promise<void> => {
+  return hdelete(uri(id), undefined, undefined, refresh ? new URLSearchParams({ refresh }) : undefined);
 };
 
 export const permission = {
