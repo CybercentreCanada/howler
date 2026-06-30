fecaa0048b3d98cc32907f97d56d304a3b479d32
Xuan Pham <xuan.pham@cyber.gc.ca>
2026-06-30 08:55:41 -0400

    copy bulk operation from assemblyline

--- api/howler/datastore/bulk.py
+++ api/howler/datastore/bulk.py
@@ -1,12 +1,13 @@
 import json
 from copy import deepcopy
+from typing import List, Optional
 
 
 class ElasticBulkPlan(object):
-    def __init__(self, indexes, model=None):
+    def __init__(self, indexes: List[str], model: Optional[type] = None):
         self.indexes = indexes
         self.model = model
-        self.operations = []
+        self.operations: List[str] = []
 
     @property
     def empty(self):
@@ -20,45 +21,45 @@ class ElasticBulkPlan(object):
                 self.operations.append(json.dumps({"delete": {"_index": cur_index, "_id": doc_id}}))
 
     def add_insert_operation(self, doc_id, doc, index=None):
-        if isinstance(doc, self.model):
-            saved_doc = doc.as_primitives(hidden_fields=True)  # type: ignore
+        if self.model and isinstance(doc, self.model):
+            saved_doc = doc.as_primitives(hidden_fields=True)
         elif self.model:
-            saved_doc = self.model(doc).as_primitives(hidden_fields=True)  # type: ignore
+            saved_doc = self.model(doc).as_primitives(hidden_fields=True)
         else:
             if not isinstance(doc, dict):
                 saved_doc = {"__non_doc_raw__": doc}
             else:
-                saved_doc = deepcopy(doc)  # type: ignore
+                saved_doc = deepcopy(doc)
         saved_doc["id"] = doc_id
 
         self.operations.append(json.dumps({"create": {"_index": index or self.indexes[0], "_id": doc_id}}))
         self.operations.append(json.dumps(saved_doc))
 
     def add_upsert_operation(self, doc_id, doc, index=None):
-        if isinstance(doc, self.model):
-            saved_doc = doc.as_primitives(hidden_fields=True)  # type: ignore
+        if self.model and isinstance(doc, self.model):
+            saved_doc = doc.as_primitives(hidden_fields=True)
         elif self.model:
-            saved_doc = self.model(doc).as_primitives(hidden_fields=True)  # type: ignore
+            saved_doc = self.model(doc).as_primitives(hidden_fields=True)
         else:
             if not isinstance(doc, dict):
-                saved_doc = {"__non_doc_raw__": doc}  # type: ignore
+                saved_doc = {"__non_doc_raw__": doc}
             else:
-                saved_doc = deepcopy(doc)  # type: ignore
+                saved_doc = deepcopy(doc)
         saved_doc["id"] = doc_id
 
         self.operations.append(json.dumps({"update": {"_index": index or self.indexes[0], "_id": doc_id}}))
         self.operations.append(json.dumps({"doc": saved_doc, "doc_as_upsert": True}))
 
     def add_update_operation(self, doc_id, doc, index=None):
-        if isinstance(doc, self.model):
-            saved_doc = doc.as_primitives(hidden_fields=True)  # type: ignore
+        if self.model and isinstance(doc, self.model):
+            saved_doc = doc.as_primitives(hidden_fields=True)
         elif self.model:
-            saved_doc = self.model(doc, mask=list(doc.keys())).as_primitives(hidden_fields=True)  # type: ignore
+            saved_doc = self.model(doc, mask=list(doc.keys())).as_primitives(hidden_fields=True)
         else:
             if not isinstance(doc, dict):
-                saved_doc = {"__non_doc_raw__": doc}  # type: ignore
+                saved_doc = {"__non_doc_raw__": doc}
             else:
-                saved_doc = deepcopy(doc)  # type: ignore
+                saved_doc = deepcopy(doc)
 
         if index:
             self.operations.append(json.dumps({"update": {"_index": index, "_id": doc_id}}))
