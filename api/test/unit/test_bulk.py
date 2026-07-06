a41debfaeedf9e0469020aed335e4f0f858347f0
Xuan Pham <xuan.pham@cyber.gc.ca>
2026-07-06 08:42:29 -0400

    change cases to test both when ops divisible and not divisible by batch size

--- api/test/unit/test_bulk.py
+++ api/test/unit/test_bulk.py
@@ -1,3 +1,5 @@
+import math
+
 import pytest
 
 from howler.datastore.bulk import ElasticBulkPlan
@@ -11,6 +13,7 @@ def operations():
         ("UPDATE", (3, {"name": "updated"})),
         ("DELETE", (4,)),
         ("INSERT", (5, {"name": "test"})),
+        ("INSERT", (6, {"name": "test"})),
     ]
 
 
@@ -19,7 +22,11 @@ def bulk_plan():
     return ElasticBulkPlan(indexes=["test_index"], model=None)
 
 
-@pytest.mark.parametrize("operation_length, batch_size", [(5, 2), (5, 3), (5, 10), (1, 2)])
+@pytest.mark.parametrize(
+    "operation_length, batch_size",
+    [(6, 2), (5, 2), (6, 10), (1, 1)],
+    ids=["divisible", "not_divisible", "larger_than_ops", "single_op"],
+)
 def test_get_plan_batches(bulk_plan, operations, operation_length, batch_size):
     for op, op_args in operations[:operation_length]:
         if op == "DELETE":
@@ -31,4 +38,5 @@ def test_get_plan_batches(bulk_plan, operations, operation_length, batch_size):
 
     batches = list(bulk_plan.get_plan_batches(batch_size=batch_size))
 
+    assert len(batches) == math.ceil(operation_length / batch_size)
     assert "".join(batches) == bulk_plan.get_plan_data()
