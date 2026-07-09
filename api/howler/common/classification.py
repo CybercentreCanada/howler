76133e8de1240f15d7639397e662f6ca68587c2a
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2026-07-09 12:47:08 -0400

    fix(api): more classification type fixes

--- api/howler/common/classification.py
+++ api/howler/common/classification.py
@@ -1,7 +1,7 @@
 import itertools
 import logging
 from copy import copy
-from typing import Any, Dict, KeysView, List, Optional, Set, Tuple, Union, cast
+from typing import TYPE_CHECKING, Any, Dict, KeysView, List, Optional, Set, Tuple, Union, cast
 
 from howler.common.exceptions import (
     HowlerKeyError,
@@ -10,6 +10,9 @@ from howler.common.exceptions import (
 )
 from howler.common.loader import APP_NAME
 from howler.odm.base import ClassificationObject
+
+if TYPE_CHECKING:
+    from howler.odm.base import ClassificationObject
 
 log = logging.getLogger(f"{APP_NAME}.classification")
 
@@ -204,12 +207,12 @@ class Classification(object):
             if not self.is_valid(classification_definition["restricted"]):
                 raise InvalidDefinition("Classification definition's restricted classification is invalid.")
 
-            self.UNRESTRICTED = self.normalize_classification(classification_definition["unrestricted"])
-            self.RESTRICTED = self.normalize_classification(classification_definition["restricted"])
+            self.UNRESTRICTED: str = self.normalize_classification(classification_definition["unrestricted"])
+            self.RESTRICTED: str = self.normalize_classification(classification_definition["restricted"])
 
         except Exception as e:
-            self.UNRESTRICTED = self.NULL_CLASSIFICATION
-            self.RESTRICTED = self.INVALID_CLASSIFICATION
+            self.UNRESTRICTED: str = self.NULL_CLASSIFICATION
+            self.RESTRICTED: str = self.INVALID_CLASSIFICATION
 
             self.invalid_mode = True
 
@@ -757,7 +760,7 @@ class Classification(object):
         )
 
     def is_accessible(
-        self, user_c12n: str | ClassificationObject, c12n: str | ClassificationObject, ignore_invalid: bool = False
+        self, user_c12n: "str | ClassificationObject", c12n: "str | ClassificationObject", ignore_invalid: bool = False
     ) -> bool:
         """
         Given a user classification, check if a user is allow to see a certain classification
