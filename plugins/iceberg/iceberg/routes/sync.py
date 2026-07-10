2f31f0ea3657de5b41d009e2085cedd2f6f7dce0
Xuan Pham <xuan.pham@cyber.gc.ca>
2026-07-10 09:07:43 -0400

    use parse_parameters instead of request.params and add docs

--- plugins/iceberg/iceberg/routes/sync.py
+++ plugins/iceberg/iceberg/routes/sync.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from flask import request
 from howler.api import make_subapi_blueprint, ok
+from howler.api.v1.utils.params import parse_parameters
 from howler.common.logging import get_logger
 from howler.common.swagger import generate_swagger_docs
 
@@ -15,12 +15,22 @@ logger = get_logger(__file__)
 
 @generate_swagger_docs()
 @iceberg_api.route("/hit_diffs", methods=["GET"])
-def get_upserted_hits():
-    """Get the hits that have been created or updated since the last sync."""
-    from_date = request.args.get("from_date")
-    to_date = request.args.get("to_date")
+@parse_parameters(from_date="required", to_date=None)
+def get_upserted_hits(*, from_date: str, to_date: str | None = None, **_extra_args):
+    """Get the hits that have been created or updated since the last sync.
 
-    last_sync_time = datetime.fromisoformat(from_date) if from_date else None
+    Arguments:
+    from_date   => The date to check for new or updated hits since, required.
+
+    Optional Arguments:
+    to_date     => The date beyond which to ignore any new or updated hits.
+
+    Result Example:
+    [
+        ...hits  # list of hits that have been created or updated since the last sync
+    ]
+    """
+    last_sync_time = datetime.fromisoformat(from_date)
     optional_end_time = datetime.fromisoformat(to_date) if to_date else None
 
     hits = [
--- plugins/iceberg/test/integration/test_sync.py
+++ plugins/iceberg/test/integration/test_sync.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 
-def test_hit_diffs(test_client, current_time):
+def test_hit_diffs_with_start_interval(test_client, current_time):
     start_time = current_time - timedelta(days=1)
 
     response = test_client.get(f"/api/v1/sync/hit_diffs?from_date={start_time.isoformat()}")
@@ -9,3 +9,22 @@ def test_hit_diffs(test_client, current_time):
     assert response.status_code == 200
     hits = response.json.get("api_response")
     assert len(hits) == 10
+
+
+def test_hit_diffs_with_start_and_end_interval(test_client, current_time):
+    start_time = current_time - timedelta(days=3)
+    end_time = current_time - timedelta(days=2, hours=1)
+
+    response = test_client.get(
+        f"/api/v1/sync/hit_diffs?from_date={start_time.isoformat()}&to_date={end_time.isoformat()}"
+    )
+
+    assert response.status_code == 200
+    hits = response.json.get("api_response")
+    assert len(hits) == 5
+
+
+def test_hit_diffs_with_no_start_interval(test_client):
+    response = test_client.get("/api/v1/sync/hit_diffs")
+
+    assert response.status_code == 400
