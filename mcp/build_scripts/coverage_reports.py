e82da4844b7239fd2d8316d8a55a62240a82358b
Matthew Rafuse <Matthew.Rafuse@cyber.gc.ca>
2026-08-11 18:44:53 -0400

    fix coverage report
    Related Issue: MCP_lucene_query

--- mcp/build_scripts/coverage_reports.py
+++ mcp/build_scripts/coverage_reports.py
@@ -6,6 +6,8 @@ import sys
 import textwrap
 from pathlib import Path
 
+newline = "\n"
+
 
 def get_color(percentage: int) -> str:
     if percentage < 50:
@@ -48,13 +50,13 @@ def main() -> None:
             # Howler MCP - Coverage Results
             ![Static Badge](https://img.shields.io/badge/Total_Coverage-{total_percentage}-{total_color}?style=flat) {diff_badge}
 
-            {diff_result}
+{newline.join([(" " * 8) + line for line in diff_result.splitlines()]) if diff_result else ""}
 
             ## Full Coverage Report
             <details>
                 <summary>Expand</summary>
 
-            {report_result}
+{newline.join([(" " * 12) + line for line in report_result.splitlines()])}
             </details>
             """
         ).strip()
