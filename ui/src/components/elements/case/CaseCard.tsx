7441b3559fd54e3b4ca1af139069ac35fde03d91
cccs-tsha <taha.shahid@cse-cst.gc.ca>
2026-09-24 08:53:51 -0400

    Fixing formatting

--- ui/src/components/elements/case/CaseCard.tsx
+++ ui/src/components/elements/case/CaseCard.tsx
@@ -1,31 +1,17 @@
-import { HourglassBottom, UpdateOutlined } from "@mui/icons-material";
-import {
-  Chip,
-  Divider,
-  Skeleton,
-  Stack,
-  Tooltip,
-  Typography,
-  useTheme,
-  type CardProps,
-} from "@mui/material";
-import api from "api";
-import StatusIcon from "components/elements/case/StatusIcon";
-import HowlerAvatar from "components/elements/display/HowlerAvatar";
-import useMyApi from "components/hooks/useMyApi";
-import dayjs from "dayjs";
-import type { Case } from "models/entities/generated/Case";
-import { useEffect, useState, type FC } from "react";
-import { twitterShort } from "utils/utils";
-import HowlerCard from "../display/HowlerCard";
+import { HourglassBottom, UpdateOutlined } from '@mui/icons-material';
+import { Chip, Divider, Skeleton, Stack, Tooltip, Typography, useTheme, type CardProps } from '@mui/material';
+import api from 'api';
+import StatusIcon from 'components/elements/case/StatusIcon';
+import HowlerAvatar from 'components/elements/display/HowlerAvatar';
+import useMyApi from 'components/hooks/useMyApi';
+import dayjs from 'dayjs';
+import type { Case } from 'models/entities/generated/Case';
+import { useEffect, useState, type FC } from 'react';
+import { twitterShort } from 'utils/utils';
+import HowlerCard from '../display/HowlerCard';
 
-const STATUS_COLORS: Partial<
-  Record<
-    string,
-    "primary" | "secondary" | "error" | "warning" | "info" | "success"
-  >
-> = {
-  resolved: "success",
+const STATUS_COLORS: Partial<Record<string, 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success'>> = {
+  resolved: 'success'
 };
 
 const CaseCard: FC<{
@@ -47,25 +33,16 @@ const CaseCard: FC<{
 
   useEffect(() => {
     if (caseId) {
-      void dispatchApi(api.v2.case.get(caseId), { throwError: false }).then(
-        (result) => {
-          if (result) {
-            setCase(result);
-          }
-        },
-      );
+      void dispatchApi(api.v2.case.get(caseId), { throwError: false }).then(result => {
+        if (result) {
+          setCase(result);
+        }
+      });
     }
   }, [caseId, dispatchApi]);
 
   if (!_case) {
-    return (
-      <Skeleton
-        variant="rounded"
-        height={250}
-        sx={{ mb: 1 }}
-        className={className}
-      />
-    );
+    return <Skeleton variant="rounded" height={250} sx={{ mb: 1 }} className={className} />;
   }
 
   const statusColor = STATUS_COLORS[_case.status!];
@@ -78,15 +55,9 @@ const CaseCard: FC<{
         {
           p: 1,
           mb: 1,
-          borderColor: statusColor
-            ? theme.palette[statusColor].main
-            : undefined,
+          borderColor: statusColor ? theme.palette[statusColor].main : undefined
         },
-        ...(Array.isArray(slotProps?.card?.sx)
-          ? slotProps.card.sx
-          : slotProps?.card?.sx
-            ? [slotProps.card.sx]
-            : []),
+        ...(Array.isArray(slotProps?.card?.sx) ? slotProps.card.sx : slotProps?.card?.sx ? [slotProps.card.sx] : [])
       ]}
       className={className}
     >
@@ -105,34 +76,24 @@ const CaseCard: FC<{
                 <Chip
                   icon={<HourglassBottom fontSize="small" />}
                   size="small"
-                  label={
-                    twitterShort(_case.start) + " - " + twitterShort(_case.end)
-                  }
+                  label={twitterShort(_case.start) + ' - ' + twitterShort(_case.end)}
                 />
               </Tooltip>
             )}
 
             <Tooltip title={dayjs(_case.updated).toString()}>
-              <Chip
-                icon={<UpdateOutlined fontSize="small" />}
-                size="small"
-                label={twitterShort(_case.updated!)}
-              />
+              <Chip icon={<UpdateOutlined fontSize="small" />} size="small" label={twitterShort(_case.updated!)} />
             </Tooltip>
           </Stack>
           <Typography variant="caption" color="textSecondary">
-            {_case.summary?.trim().split("\n")[0]}
+            {_case.summary?.trim().split('\n')[0]}
           </Typography>
           {(_case.participants?.length ?? 0) > 0 && (
             <>
               <Divider flexItem />
               <Stack direction="row" spacing={1}>
-                {_case.participants?.map((participant) => (
-                  <HowlerAvatar
-                    key={participant}
-                    sx={{ height: "20px", width: "20px" }}
-                    userId={participant}
-                  />
+                {_case.participants?.map(participant => (
+                  <HowlerAvatar key={participant} sx={{ height: '20px', width: '20px' }} userId={participant} />
                 ))}
               </Stack>
             </>
